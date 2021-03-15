06903a4713be7a0321359f463cc1ff83c2dd5c13
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-15 16:33:00 +0100

    Formik enabled for ContactForm conditionally with itemId

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import './contact-form.scss';
 import {Link} from 'react-router-dom';
+import { useFormik } from 'formik';
 
 const ContactForm = ({itemId=''}) => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            lastname: '',
+            phone: '',
+            email: '',
+            comments: ''
+        },
+        onSubmit: (values, { resetForm }) => {
+
+            if (itemId) {
+                values.itemId = itemId;
+            }
+            console.log(values);
+                 
+        },
+      });
 
     return(
         <section>
             <h3>Contact us</h3>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <div className="form-group">
                     {itemId ? <input type="hidden" className="form-control" name="id" value={itemId} /> : null}    
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" name="name" placeholder="Name" />
+                    <input type="text" className="form-control" name="name" placeholder="Name" required
+                        onChange={formik.handleChange} value={formik.values.name} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" name="lastname" placeholder="Last name" />
+                    <input type="text" className="form-control" name="lastname" placeholder="Last name" 
+                        onChange={formik.handleChange} value={formik.values.lastname} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" name="phone" placeholder="Phone" />
+                    <input type="text" className="form-control" name="phone" placeholder="Phone"
+                        onChange={formik.handleChange} value={formik.values.phone} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" name="email" placeholder="Email" />
+                    <input type="text" className="form-control" name="email" placeholder="Email"  required
+                        onChange={formik.handleChange} value={formik.values.email} />
                 </div>
                 <div className="form-group">
-                    <textarea className="form-control" name="comments" placeholder="Comments "/>
+                    <textarea className="form-control" name="comments" placeholder="Comments "
+                        onChange={formik.handleChange} value={formik.values.comments} />
                 </div>
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
