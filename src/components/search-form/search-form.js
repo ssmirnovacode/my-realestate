d07aad37becde96a4e04e30d18f960074ac091bb
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-18 16:31:13 +0100

    BAD filterfix attempt

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -12,18 +12,7 @@ const SearchForm = (props) => {
     const {province, comarca, city} = props.activeFilters;
 
     let filtersObj = {...props.activeFilters};
-    console.log(filtersObj);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.history.push('/search');
-        setFilters(filtersObj);
-    }
-
-    const handleChange = (name, value) => {
-        filtersObj[name] = value;
-        setFilters(filtersObj);
-    }
+    //console.log(filtersObj);
 
     //the following functions to be outsourced into a separate file
     const getComarcas = (arr) => {
@@ -40,20 +29,50 @@ const SearchForm = (props) => {
     const comarcas = getComarcas(items);
     const cities = getCities(items);
 
+//Event handlers
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.history.push('/search');
+        setFilters(filtersObj);
+    }
+
+    const handleProvinceChange = (name, value) => {
+        filtersObj[name] = value;
+        filtersObj.comarca = handleComarcaChange(comarca, getComarcas(items)[0])
+        console.log(filtersObj);
+        setFilters(filtersObj);
+        console.log(props.activeFilters);
+    }
+
+    const handleComarcaChange = (name, value) => {
+        filtersObj[name] = value;
+        filtersObj.city = handleCityChange(city, getCities(items)[0]);
+        console.log(filtersObj);
+        setFilters(filtersObj);
+        console.log(props.activeFilters);
+    }
+
+    const handleCityChange = (name, value) => {
+        filtersObj[name] = value;
+        setFilters(filtersObj);
+        console.log(props.activeFilters);
+    }
+
+    
     return(
         <div className="container mt-2 mb-2">
             <h2>Luxury homes for {type}</h2>
             <form onSubmit={(e) => handleSubmit(e)}>
                 <div className="input-group mb-3">
                     <select className="custom-select mr-1" name="province"
-                    onChange={(e) => handleChange(e.target.name, e.target.value)}>
+                    onChange={(e) => handleProvinceChange(e.target.name, e.target.value)}>
                         <option value="Barcelona">Barcelona</option>
                         <option value="Tarragona">Tarragona</option>
                         <option value="Lleida">Lleida</option>
                         <option value="Girona">Girona</option>
                     </select>
                     <select className="custom-select mr-1" name="comarca"
-                    onChange={(e) => handleChange(e.target.name, e.target.value)}>
+                    onChange={(e) => handleComarcaChange(e.target.name, e.target.value)}>
                         {
                             comarcas.map( com => {
                                 return(
@@ -65,7 +84,7 @@ const SearchForm = (props) => {
                     </select>
                     {/* <input type="text" className="form-control mr-1" name="city" placeholder="City" /> */}
                     <select className="custom-select mr-1" name="city"
-                    onChange={(e) => handleChange(e.target.name, e.target.value)}>
+                    onChange={(e) => handleCityChange(e.target.name, e.target.value)}>
                         {
                             cities.map( city => {
                                 return(
