84e4778228c0c06ff260d991846370308c463d80
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-26 20:03:35 +0100

    Agency name changed, geography corrected

--- src/pages/contact-page/contact-page.js
+++ src/pages/contact-page/contact-page.js
@@ -11,7 +11,7 @@ const ContactPage = () => {
                 <div className="col-12 col-md-9">
                     <section>
                         <h2>Want to sell or rent your property?</h2>
-                        <p>In aProperties we handle the entire process of advising on the sale or rental of your property. From the moment we offer our expertise through a professional, committed and effective team.
+                        <p>In MyRealEstate we handle the entire process of advising on the sale or rental of your property. From the moment we offer our expertise through a professional, committed and effective team.
                             We offer a comprehensive service for the sale or rental of your property is a success.
                         </p>
                     </section>
--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -37,7 +37,7 @@ class Home extends Component {
                         <h2 className="mb-3">Luxury Real Estate</h2>
                         <h5 className="mb-3">Four catalan provinces</h5>
                         <p>
-                        aProperties has an extensive portfolio of properties in the very best locations in the provinces of Barcelona, Tarragona, Lleida and Girona.
+                        MyRealEstate has an extensive portfolio of properties in the very best locations in the provinces of Barcelona, Tarragona, Lleida and Girona.
                         
                         </p>
                         <p>Best real estate at the most convenient prices only a click away!</p>
@@ -47,25 +47,25 @@ class Home extends Component {
                 <div className="row home">
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item ">
                         <Link to={`${basePath}/buy`} onClick={() => this.handleImgLinkClick("Barcelona")}>
-                            <img src="https://cw-gbl-gws-prod.azureedge.net/-/media/cw/emea/spain/offices/hero-image-emea-offices-barcelona-small-750x480.jpg?rev=52c4d652a22845d6a53f001f0f16218e" alt="Barcelona"/>
+                            <img src="https://b.radikal.ru/b38/2103/09/a5d19f76b108.jpg" alt="Barcelona"/>
                             <div className="home provinces-block_item_title">Barcelona</div>
                         </Link>
                     </div>
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item ">
                         <Link to={`${basePath}/buy`} onClick={() => this.handleImgLinkClick("Tarragona")}>
-                            <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/12/a6/3d.jpg" alt="Tarragona" />
+                            <img src="https://b.radikal.ru/b00/2103/56/7d9ffb5a917e.jpg" alt="Tarragona" />
                             <div className="home provinces-block_item_title">Tarragona</div>
                         </Link>
                     </div>
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item">
                         <Link to={`${basePath}/buy`} onClick={() => this.handleImgLinkClick("Lleida")}>
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Lleida_-_panoramio_-_Jorge_Franganillo.jpg" alt="Lleida" />
+                            <img src="https://b.radikal.ru/b28/2103/7b/11338d0bf154.jpg" alt="Lleida" />
                             <div className="home provinces-block_item_title">Lleida</div>
                         </Link>
                     </div>
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item">
                         <Link to={`${basePath}/buy`} onClick={() => this.handleImgLinkClick("Girona")}>
-                            <img src="https://cdn.britannica.com/35/190035-050-C44235DF/Girona-Spain-Onar-River.jpg" alt="Girona" />
+                            <img src="https://b.radikal.ru/b31/2103/5a/394765249676.jpg" alt="Girona" />
                             <div className="home provinces-block_item_title">Girona</div>
                         </Link>
                     </div>
@@ -83,11 +83,11 @@ class Home extends Component {
                     <div className="col-12 col-md-6">
                         <h4>myRealEstate</h4>
                         <p>
-                        aProperties is an estate agency specialized in the marketing of exclusive properties in the best areas of Barcelona, Madrid and Valencia. The brand also has a division specializing in the marketing of offices in prime areas of Barcelona and Madrid.
+                        MyRealEstate is an estate agency specialized in the marketing of exclusive properties in the best areas of Barcelona, Tarragona, Lleida and Girona. The brand also has a division specializing in the marketing of offices in prime areas of Barcelona and Tarragona.
 
                         As an expert consultancy, the firm has a wide portfolio of properties and a sales team made up of highly qualified, specialized professionals with extensive experience in the industry.
 
-                        aProperties offers a close-at-hand and high-quality service, where its customers can closely follow the entire process required in the purchase, sale or rental of property. 
+                        MyRealEstate offers a close-at-hand and high-quality service, where its customers can closely follow the entire process required in the purchase, sale or rental of property. 
 
                         
                         </p>
