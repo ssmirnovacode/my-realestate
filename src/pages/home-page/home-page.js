ca51f91eb92ed127830ade40c540c368e6480e73
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-26 17:46:27 +0100

    Links to BuyPage from HomePage fixed

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -6,6 +6,7 @@ import {itemsLoaded, itemsRequested, itemsError, setDeal} from '../../redux/acti
 import ItemsView from '../../components/items-view/items-view';
 import RequestService from '../../services/requests';
 import baseURL from '../../assets/baseURL';
+import basePath from '../../assets/basePath';
 
 const reqService = new RequestService();
 
@@ -39,25 +40,25 @@ class Home extends Component {
 
                 <div className="row home">
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item ">
-                        <Link to="/buy">
+                        <Link to={`${basePath}/buy`}>
                             <img src="https://cw-gbl-gws-prod.azureedge.net/-/media/cw/emea/spain/offices/hero-image-emea-offices-barcelona-small-750x480.jpg?rev=52c4d652a22845d6a53f001f0f16218e" alt="Barcelona"/>
                             <div className="home provinces-block_item_title">Barcelona</div>
                         </Link>
                     </div>
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item ">
-                        <Link to="/buy">
+                        <Link to={`${basePath}/buy`}>
                             <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/12/a6/3d.jpg" alt="Tarragona" />
                             <div className="home provinces-block_item_title">Tarragona</div>
                         </Link>
                     </div>
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item">
-                        <Link to="/buy">
+                        <Link to={`${basePath}/buy`}>
                             <img src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Lleida_-_panoramio_-_Jorge_Franganillo.jpg" alt="Lleida" />
                             <div className="home provinces-block_item_title">Lleida</div>
                         </Link>
                     </div>
                     <div className="col-12 col-md-6 col-xl-3 home provinces-block_item">
-                        <Link to="/buy">
+                        <Link to={`${basePath}/buy`}>
                             <img src="https://cdn.britannica.com/35/190035-050-C44235DF/Girona-Spain-Onar-River.jpg" alt="Girona" />
                             <div className="home provinces-block_item_title">Girona</div>
                         </Link>
