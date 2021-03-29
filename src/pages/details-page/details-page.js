d8d18b327b0e15a3c06427322719b4a64c004a2f
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-29 16:22:32 +0200

    Map embedded into Details conditioned to city

--- src/pages/details-page/details-page.js
+++ src/pages/details-page/details-page.js
@@ -13,21 +13,6 @@ class DetailsPage extends Component {
 
         const {...itemProps} = targetItem;
 
-        /* const initMap = (lat, lng) => {
-            // The location of Uluru
-            const uluru = { lat: -25.344, lng: 131.036 };
-            // The map, centered at Uluru
-            const map = new google.maps.Map(document.getElementById("map"), {
-              zoom: 4,
-              center: uluru,
-            });
-            // The marker, positioned at Uluru
-            const marker = new google.maps.Marker({
-              position: uluru,
-              map: map,
-            });
-          } */
-
         return(
             <div className="container details">
                 <div className="row">
@@ -35,22 +20,14 @@ class DetailsPage extends Component {
                         <div className="details-prop-header mb-2">Luxury {targetItem.type} for {this.props.deal}</div>
                         <div className="details-prop-basicdata mb-2">{targetItem.price} &#8364; | {targetItem.surface} m<sup>2</sup> | {targetItem.bedrooms} bedrooms | {targetItem.bathrooms} bathrooms</div>
                         <PropertyCard {...itemProps} slider classnames="detail"/>
-                    </div>
-                    
-                    <div className="contact-form-details col-12 col-md-3">
-                        <h2>{targetItem.title}</h2>
-                        <RequestInfo itemId={itemId} formId="request-info"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12" id="map">
-                        <div className = "mapouter" > 
+                        
+                        <div className = "mapouter details" > 
                             <div className = "gmap_canvas" > 
                                 <iframe width = "600"
                                     title="maps-instance"
                                     height = "500"
                                     id = "gmap_canvas"
-                                    src = "https://maps.google.com/maps?q=passeig%20de%20gracia%2015&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                                    src = {`https://maps.google.com/maps?q=${targetItem.city}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
                                     frameBorder = "0"
                                     scrolling = "no"
                                     marginHeight = "0"
@@ -58,12 +35,18 @@ class DetailsPage extends Component {
                                 </iframe><br/>
                             </div>
                         </div>
+                    </div>                   
+                    <div className="contact-form-details col-12 col-md-3">
+                        <h2>{targetItem.title}</h2>
+                        <RequestInfo itemId={itemId} formId="request-info"/>
                     </div>
                 </div>
             </div>
         )
     }   
 }
+
+/* "https://maps.google.com/maps?q=tarragona&t=&z=13&ie=UTF8&iwloc=&output=embed" */
 
 const mapStateToProps = (state) => ({
     items: state.items,
