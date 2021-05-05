1b282a662db648a10b59e0707662207d30403669
ssmirnova <svetlana.smirnova213@gmail.com>
2021-05-05 17:34:13 +0200

    Nav menu refactored

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navigation.scss';
 import {Link} from 'react-router-dom';
 import basePath from '../../assets/basePath';
 
+const menuItems = [
+    {
+        id: 0,
+        label: 'Home',
+        link: ''
+    },
+    {
+        id: 1,
+        label: 'Buy',
+        link: 'buy'
+    },
+    {
+        id: 2,
+        label: 'Sell',
+        link: 'sell'
+    },
+    {
+        id: 3,
+        label: 'Rent',
+        link: 'rent'
+    },
+    {
+        id: 4,
+        label: 'About us',
+        link: 'about'
+    },
+    {
+        id: 5,
+        label: 'Contact us',
+        link: 'contact'
+    }
+];
+
 const Navigation = (props) => {
+
+    const [activeItem, setActiveItem] = useState(0);
 
     const handleMenuItemClick = (e) => {
         if (e.target.tagName === 'A') {
-            document.getElementById('menu-toggle-btn').click();
+            document.getElementById('menu-toggle-btn').click(); //closing the burger menu when a menu link is clicked
         }
     };
 
-    const handleNavItemActiveChange = (e) => {
-       document.querySelectorAll('.mainnav').forEach(item => {
-           item.classList.remove('activeItem');
-       });
-       if (e.target.tagName === "A") {
-            e.target.parentNode.classList.add('activeItem');
-       }
-       else if (e.target.tagName === "LI") {
-           e.target.classList.add('activeItem');
-       }
+    const handleNavItemActiveChange = (id) => {
+        setActiveItem(id);
     } 
 
     return(
@@ -28,24 +55,15 @@ const Navigation = (props) => {
                 <Link className="nav-link" to={`${basePath}/`}><h1>My Real Estate</h1></Link>               
 
                 <ul className="nav nav-pills ml-auto">
-                    <li className="nav-item mainnav activeItem" onClick={(e) => handleNavItemActiveChange(e)}>
-                        <Link className="nav-link" to={`${basePath}/`}>Home</Link>
-                    </li>
-                    <li className="nav-item mainnav" onClick={(e) => handleNavItemActiveChange(e)}>
-                        <Link className="nav-link" to={`${basePath}/buy`} >Buy</Link>
-                    </li>
-                    <li className="nav-item mainnav" onClick={(e) => handleNavItemActiveChange(e)}>
-                        <Link className="nav-link" to={`${basePath}/sell`} >Sell</Link>
-                    </li>
-                    <li className="nav-item mainnav" onClick={(e) => handleNavItemActiveChange(e)}>
-                        <Link className="nav-link"  to={`${basePath}/rent`}>Rent</Link>
-                    </li>
-                    <li className="nav-item mainnav" onClick={(e) => handleNavItemActiveChange(e)}>
-                        <Link className="nav-link" to={`${basePath}/about`} >About us</Link>
-                    </li>
-                    <li className="nav-item mainnav" onClick={(e) => handleNavItemActiveChange(e)}>
-                        <Link className="nav-link" to={`${basePath}/contact`} >Contact us</Link>
-                    </li>                   
+                    {
+                        menuItems.map(item => {
+                            return(
+                                <li key={item.id} id={item.id} className={activeItem === item.id ? "nav-item mainnav activeItem" : "nav-item mainnav"} onClick={() => handleNavItemActiveChange(item.id)}>
+                                    <Link className="nav-link" to={`${basePath}/${item.link}`}>{item.label}</Link>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
 
                 <button id="menu-toggle-btn" className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -54,24 +72,16 @@ const Navigation = (props) => {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav" onClick={(e) => handleMenuItemClick(e)}>
-                        <li className="nav-item active">
-                            <Link className="nav-link drawer"  to={`${basePath}/`}>Home <span className="sr-only">(current)</span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link drawer"  to={`${basePath}/buy`}>Buy</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link drawer" to={`${basePath}/sell`} >Sell</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link drawer" to={`${basePath}/rent`} >Rent</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link drawer" to={`${basePath}/about`} >About us</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link drawer" to={`${basePath}/contact`} >Contact us</Link>
-                        </li>                   
+                        {
+                            menuItems.map(item => {
+                                return(
+                                    <li key={item.id} id={item.id} className={activeItem === item.id ? "nav-item active" : "nav-item"} >
+                                        <Link className="nav-link drawer" to={`${basePath}/${item.link}`}  onClick={() => handleNavItemActiveChange(item.id)}>{item.label} 
+                                        {activeItem === item.id ? <span className="sr-only">(current)</span> : null}   </Link>
+                                    </li>
+                                )
+                            })
+                        }            
                     </ul>
                 </div>
 
